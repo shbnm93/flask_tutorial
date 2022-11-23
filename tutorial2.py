c03d19e0483b889f60c54ad0f71ca8f597dc8b74
shbnm93 <shabnamhamidi1993@gmail.com>
2022-11-23 13:47:21 +0300

    flask tutorials in progress

---
+++ tutorial2.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=["Ross", "Monica", "Rachel", "Joie", "Chandler", "Phebe"])
+
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name="Admin!"))
+
+
+
+if __name__ == "__main__":
+    app.run()
